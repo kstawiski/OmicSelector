ac6602076aa3cbafb2f4a8781975a27955d73a79
Konrad Stawiski <konrad@konsta.com.pl>
2020-10-05 21:10:35 +0000

    Dockerfiles update

--- docker/keras.R
+++ docker/keras.R
@@ -5,7 +5,7 @@ options(repos = r)
 
 install.packages('devtools')
 devtools::install_github('rstudio/keras')
-install.packages('reticulate')
+install.packages(c('reticulate','opencv'))
 reticulate::use_python('/opt/conda/bin/python')
 
 require(tensorflow)
