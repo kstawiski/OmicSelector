bc7cb958fa3dacdf501ac25c26b89b56e0d97c64
Konrad Stawiski <konrad@konsta.com.pl>
2021-03-27 21:58:31 +0000

    codespaces update

--- R/OmicSelector_log.R
+++ R/OmicSelector_log.R
@@ -8,14 +8,14 @@
 #'
 #' @export
 OmicSelector_log = function(message_to_log = "Working...", logfile = "temp.log") {
-  try({
   hostname = as.character(R.utils::GString$getBuiltinHostname())
   timestamp = as.character(Sys.time())
   pid = as.character(R.utils::getBuiltinPid.GString())
-  sink(logfile, append=TRUE)
   to_write = paste0("\n[",timestamp," | pid:", pid,"] ", message_to_log)
-  cat(to_write)
-  sink()
-  message(to_write)
+  try({
+    sink(logfile, append=TRUE)
+    cat(to_write)
+    sink()
   })
+  message(to_write)
 }
