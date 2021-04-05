e6ae7fcf2298134f6b09bc32416ec311a68ad717
Konrad Stawiski <konrad@konsta.com.pl>
2021-04-05 10:56:44 +0000

    header test

--- static/index.php
+++ static/index.php
@@ -159,10 +159,10 @@ $(document).ready(function(e){
         <div class="starter-template">
             <p>
             <table border="0" cellspacing="0" cellpadding="0">
-            <tr><td style="width: 50%;"><a href="/"><center><img src="logo.png" width="50%" /></center></a></td><td style="width: 50%;">
-            <p>Host: <code><?php echo gethostname() . " (" . $_SERVER['REMOTE_ADDR'] . ")"; ?></code></p>
-            <p>Reference: <a href="https://biostat.umed.pl/OmicSelector" target="_blank">https://biostat.umed.pl/OmicSelector</a></p>
-            <p>Version: <code>OmicSelector v1.0.<?php if (file_exists('/version.txt')) { $version = file_get_contents('/version.txt'); } else { $version = "init"; }; echo $version; ?></code></p>
+            <tr><td style="width: 70%;"><a href="/"><center><img src="logo.png" width="50%" /></center></a></td><td style="width: 30%; float:right;text-align:right;">
+            <p style="font-size: small;">Host: <code><?php echo gethostname() . " (" . $_SERVER['REMOTE_ADDR'] . ")"; ?></code></p>
+            <p style="font-size: small;">Reference: <a href="https://biostat.umed.pl/OmicSelector" target="_blank">https://biostat.umed.pl/OmicSelector</a></p>
+            <p style="font-size: small;">Version: <code>OmicSelector v1.0.<?php if (file_exists('/version.txt')) { $version = file_get_contents('/version.txt'); } else { $version = "init"; }; echo $version; ?></code></p>
             </td></tr></table>
             </p>
         </div>
